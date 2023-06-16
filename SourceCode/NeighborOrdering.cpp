76ea5b3fcfcf64e86f7422f186462deea98ffae1
Wasif478 <69196895+Wasif478@users.noreply.github.com>
2023-06-16 12:55:26 -0400

    NeighborOrdering.cpp

--- SourceCode/NeighborOrdering.cpp
+++ SourceCode/NeighborOrdering.cpp
@@ -3,7 +3,7 @@
 namespace Tessellation {
 	//-------- Function to sort the list of neighbors (using the global list of neighbors- "pos_neighbor") according to its angle with the target particle \n //
 	// and an arbitrary direction (vector between target particle and the first neighbor) ...................//
-	vector<int> neighborordering(int i, vector<vector<int>>pos_neighbor, vector<Vector3d>pos) {
+	vector<int> neighborordering(int i, vector<vector<int>>&pos_neighbor, vector<Vector3d>&pos) {
 		if (pos_neighbor[i].size() == 1) {
 			vector<int>finall;
 			finall.push_back(pos_neighbor[i][0]);
@@ -111,7 +111,7 @@ namespace Tessellation {
 
 
 	//-------- Same function as above but the list of neigbhors is provided through "givencontainer" instead of using the global list .........//
-	vector<int> neighborordering(int i, vector<int>givencontainer, vector<vector<int>>pos_neighbor, vector<Vector3d>pos) {
+	vector<int> neighborordering(int i, vector<int>&givencontainer, vector<vector<int>>&pos_neighbor, vector<Vector3d>&pos) {
 		vector<int>finallist;
 		int currentnode = i;
 		Real isize = givencontainer.size();
