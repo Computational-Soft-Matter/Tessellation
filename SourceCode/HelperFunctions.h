6ba8fd02d56552a9a6645b8a1322eccb34160127
Wasif478 <69196895+Wasif478@users.noreply.github.com>
2023-06-16 12:55:40 -0400

    HelperFunctions.h

--- SourceCode/HelperFunctions.h
+++ SourceCode/HelperFunctions.h
@@ -8,10 +8,10 @@ namespace Tessellation {
 	Real dist(Vector3d A, Vector3d B);
 
 	//----------------Vector search for 'a' inside Vector A-----------------//
-	int vector_search(vector<int>A, int a);
+	int vector_search(vector<int>&A, int a);
 
 	//----------------Vector search to find index of key--------//
-	int find_ind(vector<int>A, int a);
+	int find_ind(vector<int>&A, int a);
 
 	//------------Circumcenter for a triangle with vertices given-------//
 	Vector3d findcc(Vector3d A, Vector3d B, Vector3d C);
@@ -28,11 +28,11 @@ namespace Tessellation {
 	vector<Real> sort(map<Real, Real>& M);
 
 	//-------------Debugging Functions---------------//
-	void print_vector(vector<int>A);
+	void print_vector(vector<int>&A);
 
-	void print_vector(vector<Real>A);
+	void print_vector(vector<Real>&A);
 
-	void print_vector(vector<pair<int, int>>A);
+	void print_vector(vector<pair<int, int>>&A);
 
 	void test();
 
