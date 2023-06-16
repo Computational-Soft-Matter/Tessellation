d40a890ceaa95742661d5789d99ab15e2f30a0f6
Wasif478 <69196895+Wasif478@users.noreply.github.com>
2023-06-16 12:55:12 -0400

    NeighborOrdering.h

--- SourceCode/NeighborOrdering.h
+++ SourceCode/NeighborOrdering.h
@@ -7,10 +7,10 @@
 namespace Tessellation {
 	//-------- Function to sort the list of neighbors (using the global list of neighbors- "pos_neighbor") according to its angle with the target particle \n //
 	// and an arbitrary direction (vector between target particle and the first neighbor) ...................//
-	vector<int> neighborordering(int i, vector<vector<int>>pos_neighbor, vector<Vector3d>pos);
+	vector<int> neighborordering(int i, vector<vector<int>>&pos_neighbor, vector<Vector3d>&pos);
 
 	//-------- Same function as above but the list of neigbhors is provided through "givencontainer" instead of using the global list .........//	
-	vector<int> neighborordering(int i, vector<int>givencontainer, vector<vector<int>>pos_neighbor, vector<Vector3d>pos);
+	vector<int> neighborordering(int i, vector<int>&givencontainer, vector<vector<int>>&pos_neighbor, vector<Vector3d>&pos);
 };
 
 #endif // _NEIGHBORORDERING_H_
